925805a64b1f4757a5275559dadfcdccc503bf7e
Coretchi Mihai <prog.eamtc@gmail.com>
2025-10-27 22:45:22 +0200

    Init database

--- monolithic/market/src/app/app.module.ts
+++ monolithic/market/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import configuration from 'src/config/configuration';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,21 @@ import { UserModule } from 'src/user/user.module';
       isGlobal: true,
       envFilePath: `${process.cwd()}/env/.env.${process.env.NODE_ENV}`,
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('database.host'),
+          port: config.get<number>('database.port'),
+          username: config.get<string>('database.username'),
+          password: config.get<string>('database.password'),
+          database: config.get<string>('database.database'),
+          synchronize: true,
+          entities: [],
+        };
+      },
     }),
     UserModule,
   ],
