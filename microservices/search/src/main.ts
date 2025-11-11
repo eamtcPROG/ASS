39f5ae37bb2d9b0f21ab65ebfec7a04a9ec98cb7
Coretchi Mihai <prog.eamtc@gmail.com>
2025-11-12 01:14:57 +0200

    Init search service

--- microservices/search/src/main.ts
+++ microservices/search/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+  if (!port) {
+    throw new Error('PORT is not set');
+  }
+  const version = configService.get<string>('version');
+  if (!version) {
+    throw new Error('version is not set');
+  }
+  const config = new DocumentBuilder()
+    .setTitle('API Search Service')
+    .setDescription(
+      'HTTP API for the Search service. Manage Searchs and other Search-related operations with versioned endpoints, validation, and standard error responses.',
+    )
+    .addBearerAuth(
+      {
+        description: `[just text field] Please enter token in following format: Bearer `,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+      },
+      'jwt',
+    )
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  await app.listen(port);
 }
-bootstrap();
+void bootstrap();
