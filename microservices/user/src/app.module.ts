6c872dbbaef0b623db184df582dfb81966f298e4
Coretchi Mihai <prog.eamtc@gmail.com>
2025-11-11 23:21:15 +0200

    Init user service

--- microservices/user/src/app.module.ts
+++ microservices/user/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/env/.env.${process.env.NODE_ENV}`,
+      load: [configuration],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ microservices/user/src/config/configuration.ts
@@ -0,0 +1,17 @@
+export default () => ({
+  NODE_ENV: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  version: process.env.VERSION,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+
+  jwt: {
+    secret: process.env.JWT_SECRET ?? 'secret',
+    expires_in: parseInt(process.env.JWT_EXPIRES_IN ?? '3600', 10),
+  },
+});
